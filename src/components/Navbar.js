33511de0489cb77f56ba4aeea517661363ebf481
Alexander Dan <40121410+apdan510@users.noreply.github.com>
2022-09-26 21:27:03 -0700

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,7 +29,7 @@ function Navbar() {
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-            West Coast Villains
+            
             <i class="" />
           </Link>
           <div className="menu-icon" onClick={handleClick}>
